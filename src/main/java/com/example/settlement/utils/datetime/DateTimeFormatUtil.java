b8cfcf2e3fbbe25bfde35b60bdd1ac0f7d557e58
yangwu <yangwuse@mail.ustc.edu.cn>
2023-04-05 22:06:00 +0800

    添加日志

--- src/main/java/com/example/settlement/utils/datetime/DateTimeFormatUtil.java
+++ src/main/java/com/example/settlement/utils/datetime/DateTimeFormatUtil.java
@@ -1,5 +1,7 @@
 package com.example.settlement.utils.datetime;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -12,14 +14,26 @@ import java.util.Locale;
  * @author yangwu_i
  * @date 2023/4/5 20:52
  */
+@Slf4j
 public class DateTimeFormatUtil {
     public static final String YYYY_MM_DD = "yyyy-MM-dd";
     public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 
     public static String format(String countryCode, long timestamp, String pattern) {
-        ZoneId zoneId = ZoneIdByCountryCode.getZoneIdByCountryCode(countryCode);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(timestamp), zoneId);
-        return formatter.format(zonedDateTime);
+        try {
+            ZoneId zoneId = ZoneIdByCountryCode.getZoneIdByCountryCode(countryCode);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+            ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(timestamp), zoneId);
+            return formatter.format(zonedDateTime);
+        } catch (Exception e) {
+            log.error("DateTimeFormatUtil format error", e);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
     }
 }
